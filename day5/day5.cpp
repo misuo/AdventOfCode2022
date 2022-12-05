c93c76907de991677b7b732949f82515a7433fb9
misuo <21021375+misuo@users.noreply.github.com>
2022-12-05 09:46:08 +0100

    Refactor
    Refactors loading into Load() function. Added links to other C++ solutions.

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -25,6 +25,39 @@ std::ostream& operator<<(std::ostream& os, const Stacks& stacks)
   return os;
 }
 
+void Load(Stacks& stacks, Moves& moves)
+{
+  std::ifstream fin("input.txt");
+  for( std::string line; std::getline(fin, line); )
+  {
+    if( line.empty() )
+      continue;
+
+    if( line.front() == 'm' )
+    {
+      Move move;
+
+      std::string word; // dummy
+      std::istringstream istr(line);
+      istr >> word >> std::get<0>(move) >> word >> std::get<1>(move) >> word >> std::get<2>(move);
+
+      moves.push_back(move);
+    }
+    else if( line[1] != '1' )
+    {
+      const StackNo stackscount = line.length() / 4 + 1;
+      if( stacks.empty() )
+        stacks.resize(stackscount); // Create stacks
+
+      for( StackNo n = 0; n < stackscount; ++n )
+      {
+        if( CrateId ch = line[4 * n + 1]; ch != ' ' )
+          stacks[n].push_back(ch);
+      }
+    }
+  }
+}
+
 void Part1(Stacks stacks,const Moves & moves)
 {
   for( const auto& move : moves )
@@ -69,38 +102,8 @@ int main()
   Stacks stacks;
   Moves  moves;
 
-  // Load data
-  std::ifstream fin("input.txt");
-  for( std::string line; std::getline(fin, line); )
-  {
-    if( line.empty() )
-      continue;
-
-    if( line.front() == 'm' )
-    {
-      Move move;
-
-      std::string word; // dummy
-      std::istringstream istr(line);
-      istr >> word >> std::get<0>(move) >> word >> std::get<1>(move) >> word >> std::get<2>(move);
-
-      moves.push_back(move);
-    }
-    else if( line[1] != '1' )
-    {
-      const StackNo stackscount = line.length()/4+1;
-      if( stacks.empty() )
-        stacks.resize(stackscount); // Create stacks
-
-      for( StackNo n=0; n < stackscount; ++n )
-      {
-        if( CrateId ch = line[4*n+1]; ch != ' ' )
-          stacks[n].push_back(ch);
-      }
-    }
-  }
-
-  assert(stacks.size() == 9); // Verify correct number of stacks loaded.
+  Load(stacks, moves);
+  assert(stacks.size() == 9);  // Verify correct number of stacks loaded.
   assert(moves.size() == 502); // Verify correct number of moves loaded.
 
   Part1(stacks,moves);
@@ -110,3 +113,11 @@ int main()
 }
 
 // Other
+// * https://github.com/crzyhorse/AdventOfCode2022/blob/main/day5/Day5.cpp (https://old.reddit.com/r/adventofcode/comments/zcxid5/2022_day_5_solutions/iyz9g5c/)
+// * https://github.com/bobolpwnz/AdventOfCode2022/blob/main/src/days/day05.cpp
+// * https://github.com/RiotNu/advent-of-code-2022/commit/ccce490c55da9184c27e7d46218325738f9c0ce8
+// * https://github.com/vss2sn/advent_of_code/blob/master/2022/cpp/day_05a.cpp
+//   https://github.com/vss2sn/advent_of_code/blob/master/2022/cpp/day_05b.cpp
+// * https://github.com/willkill07/AdventOfCode2022/blob/main/days/Day05.hpp (https://old.reddit.com/r/adventofcode/comments/zcxid5/2022_day_5_solutions/iyz38bl/)
+//   https://github.com/willkill07/AdventOfCode2022/blob/main/days/Day05.cpp
+// * https://topaz.github.io/paste/#XQAAAQA0DAAAAAAAAAARmknGRw8TogB3HRdqOZ3QvocKtDLIs+k1aQq8nn6y4JCwKflnTwe9r+kNPXlFg6+4xjpPuczrMADyj2VqC+7v8/ZisHzTxjKEES4vMr7ssA4595/csAs1cUlLQOf8RPPgX2OAGKmS0ZvByMvQLkVFso3YYdG8iwnSZAb950dqjpjEL8OjK6v1iUHU4sku3JXzMPRPo5B5gvFV1UZwYlv05uGDaSaGYL9MmHlTYQzD04hELk8BEHp2cSzGJKgQ4RXpIM2cRd6toeIUCBYalTzYOwml09MNDwe3EJrASDljvoBtp8blW4pXluuU1t3yGT2FBkBRzvwPgrz9YqwamAkxBBc5e4JXCjqFgrEfVHq5TQQmeUUrLoHZDzqjLASO7ycdEpcSUUS6HERW6zIxGPBqlQYM92/JMkGAe5n1YNzFofIUE6QwarrZa53TRUjMYMmu4lWL1+srl6AuS1D4DoKVZWo1bgu9B7OY4d/N7FcKYHdFNcxUI+nbsJVRAxLXnD2mEf4DlN/yzMcumoRsGRYXjmfK0BuLmE69hHjNPzapR1sQPRlmCTK9/ub7JgH1nkmz263GhhlW3EDNnBpZSD1fkcwes0IRryOdxSH85W4UB9CAZpNkLlZk2vo0ri8VlLBao21PSmYNiJ8y6asPf7ca1vdiVKPUt7dUyRL8qQdNql/6snx7Ov8AFonz64avlYe8JAP8/YdyPCh4SH2ggTl9Sf9iiHW9kJv4JsPIOhJ3W1ntFS6j5wXRrJa2x7HX/aPD2GQbpU4y+LQNjI1Rg+3r1Xfdg+/Wh8DFNDhk3VZLdlSp/VIG/CTYyBmaVeQZRHzMYdZjgV7lJWgmlUHyV5mnbyQDRVH7zqe3
